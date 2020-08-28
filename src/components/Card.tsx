0a4178471a5e92bd5c68cfd26628b278aac55d61
aarnav982023 <aarnav982023@gmail.com>
2020-08-28 20:21:52 +0530

    completed card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,6 +5,7 @@ const Card: React.FC<{ college: any }> = ({ college }) => {
   return (
     <div className="card">
       <div className="college_img">
+        <div className="promoted">PROMOTED</div>
         <div className="rating">
           <div className="rating-text">{college.rating}/5</div>
           <div className="rating-remarks">{college.rating_remarks}</div>
@@ -29,6 +30,25 @@ const Card: React.FC<{ college: any }> = ({ college }) => {
             })}
           </div>
         </div>
+        <div className="original-fees-div">
+          <span className="original-fees">₹{college.original_fees}</span>
+          <div className="discount">•{college.discount}</div>
+        </div>
+        <div className="nearby-div">
+          {college.nearest_place.map((nearBy: any) => {
+            return <span className="nearby">{nearBy}</span>;
+          })}
+        </div>
+        <div className="fees">
+          <div className="discounted-fees">₹{college.discounted_fees}</div>
+          <div className="fees_cycle">{college.fees_cycle}</div>
+        </div>
+        <div className="match-div">
+          <span className="match">93% match :</span>
+          <span className="famous-nearby">{college.famous_nearest_places}</span>
+        </div>
+        <div className="offer-div">{college.offertext}</div>
+        <div className="cancellation">Free Cancellation • Free Wifi</div>
       </div>
     </div>
   );
